9eb1507bd3409c744e6ce3953c4553a8c0437508
Bruna Landim <56938713+linnep@users.noreply.github.com>
2019-12-12 17:45:51 -0300

    localstorage and json
    Consegui!!!!
    I can do it !!!

--- form-js/js/main-form.js
+++ form-js/js/main-form.js
@@ -1,68 +1,76 @@
-listaFicha = document.getElementById("tabHeroes")
+// variavéis do formulário
+var listaFicha = document.getElementsByClassName("tabHeroes"); 
+
 var formNome = document.getElementById("nome").value; 
-var formDate = document.getElementById("dataNas").value; 
+
+var formData = document.getElementById("dataNas").value;
+
 var formIdade = document.getElementById("idade").value; 
-var formPower = document.getElementById("poder").value; 
 
+var formPoder = document.getElementById("poder").value; 
 
-if(localStorage.getItem('listaFicha') === null){
-    console.log(listaFicha);
-    var listaFicha = [];
-
-} else{
-    var listaFicha = JSON.parse(localStorage.getItem('listaFicha'));
+if (localStorage.getItem("ficha_tecnica") === null){
+    console.log ('tem cadastro aqui'); 
+    console.log('tem cadastro vazio aqui' + ficha_Tecnica); 
+    var ficha_Tecnica = []; 
+}else{
+    var ficha_Tecnica = JSON.parse(localStorage.getItem("ficha_tecnica")); 
 }
 
-if(listaFicha != 0){
-    for(var i = 0; i < listaFicha.lenght; i++){
-        var lista_seq = i+1;
-        var lista_nome = listaFicha[i].formNome; 
-        var lista_date = listaFicha[i].formDate; 
-        var lista_idade = listaFicha[i].formIdade; 
-        var lista_power = listaFicha[i].formPower;  
+listaFicha[0].innerHTML += '<tr><th>Item</th><th>Nome</th><th>Data de Nascimento</th><th>Idade</th><th>Poder</th><tr>'; 
 
-        listaFicha [0] += '<tr><td>'+ lista_seq + '</td><td>' + lista_nome + '</td><td>' + lista_date + '</td><td>' + lista_idade + '</td><td>' + lista_power + '</td> <tr>'
+if(ficha_Tecnica !== 0){
+    for (var a = 0; a < ficha_Tecnica.length; a++){
+        var lista_seq = a+1; 
+
+        var lista_nome = ficha_Tecnica[a].Nome; 
+        var lista_dataNas = ficha_Tecnica[a].Data_de_Nascimento;
+        var lista_idade = ficha_Tecnica[a].Idade; 
+        var lista_poder = ficha_Tecnica[a].Poder; 
+   
+    listaFicha[0].innerHTML += '<tr><td>' + lista_seq + '</td><td>' + lista_nome + '</td><td>' + lista_dataNas + '</td><td>' + lista_idade + '</td><td>' + lista_poder + '</td><tr>'
+   
+    }
+}
+
+var salvarCadastro = document.getElementById("salvarFicha"); 
+var novaFicha = ""; 
+
+salvarCadastro.onclick = function(){
+
+    setSeq = ficha_Tecnica.length+1;
+    
+    getNome = document.getElementById("nome").value; 
+    getData = document.getElementById("dataNas").value; 
+    getIdade = document.getElementById("idade").value; 
+    getPoder = document.getElementById("poder").value; 
+        
+    // console.log(getNome);
+
+    novaFicha = {
+        'Nome': getNome, 
+        'Data de Nascimento': getData,
+        'Idade': getIdade,
+        'Poder': getPoder
+    }
+
+    console.log('valor do set =' + setSeq); 
+
+    ficha_Tecnica.push(novaFicha); 
+
+    listaFicha[0].innerHTML += '<tr><td>' + setSeq + '</td><td>' + getNome + '</td><td>' + getData + '</td><td>' + getIdade + '</td><td>' + getPoder + '</td></tr>'; 
+
+    var renovaInserDados = document.getElementById("fichaTecnica"); 
+
+    renovaInserDados.onreset = function()
+    {
+        ficha_tecnica_json = JSON.stringify(ficha_Tecnica); 
+
+    window.localStorage.setItem('ficha_tecnica', ficha_tecnica_json)
 
     }
 
-
-
-}
-
-function salvarFicha(novaFicha){
-
-setSeq = listaFicha.length + 1; 
-
-getNome = document.getElementById("nome").value; 
-getData = document.getElementById("dataNas").value; 
-getIdade = document.getElementById("idade").value; 
-getPoder = document.getElementById("poder").value; 
-
-novaFicha = {
-    "Nome": getNome, 
-    "Data": getData, 
-    "Idade": getIdade,
-    "Poder": getPoder
-}
-
-novaFicha.push(listaFicha);
-
-var renovarInsertdados = document.getElementById("fichaTecnica"); 
-listaFicha [0].innerHTML += '<tr><td>' + getformNome + '</td><td>' + getformDate + '</td><td>' + getformIdade + '</td><td>' + getformPower + '</td> <tr>'
-
-renovarInsertdados.onreset = function(){
-    listaFicha_json = JSON.stringify(listaFicha); 
-    window.localStorage.setItem("listaFicha", listaFicha_json); 
+    document.getElementById('fichaTecnica').reset(); 
+    return false; 
 
 }
-
-document.getElementById("fichaTecnica")
-
-
-return false; 
-
-}
-
-// listaFicha[0].innerHTML += '<tr><th>Nome</th><th>Data</th><th>Idade</th><th>Poder</th>'; 
-
-
